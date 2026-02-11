dec017e2d9c72955a5d8e133100bb11e24ff2765
Cap-Bebop <257469859+Cap-Bebop@users.noreply.github.com>
2026-02-11 17:20:55 +0800

    Update touch_ff_manager.h
    change default haptics  
    magnitude from 1.0 to 0.5  
    duration from 6 to 4

--- haptic/touch_ff_manager.h
+++ haptic/touch_ff_manager.h
@@ -22,9 +22,9 @@ enum class TouchKeyboardEvent {
 };
 
 // Default magnitude for haptic feedback.
-constexpr double kDefaultHapticMagnitude = 1.0;
+constexpr double kDefaultHapticMagnitude = 0.5;
 // Default duration for haptic feedback in ms.
-constexpr int kDefaultHapticDurationMs = 6;
+constexpr int kDefaultHapticDurationMs = 4;
 
 class TouchFFManager {
   /* TouchFFManager (touch force feeback manager) class manages the haptic
